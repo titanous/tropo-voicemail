74c0c11e91eecc52a391f8cb46917b2df369bad1
Jonathan Rudenberg <jonathan@titanous.com>
2010-05-04 13:42:42 -0400

    switch to bundler

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :gemcutter
+gem 'sinatra'
+gem 'nokogiri'
+gem 'pony', :git => 'http://github.com/benprew/pony.git'
--- voicemail.rb
+++ voicemail.rb
@@ -1,6 +1,17 @@
+begin
+  # Require the preresolved locked set of gems.
+  require ::File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fallback on doing the resolve at runtime.
+  require 'rubygems'
+  require 'bundler'
+  Bundler.setup
+end
+
 require 'sinatra'
-require 'whitepages_lookup'
 require 'pony'
+require 'whitepages_lookup'
+
 
 configure do
   SENDGRID_CONFIG = {
